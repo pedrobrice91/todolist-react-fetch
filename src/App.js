82f1199c22040cd315d9dc21f8be7c3612184913
Pedro L Briceno <66653154+pedrobrice91@users.noreply.github.com>
2024-07-20 11:33:41 -0400

    second  commit

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ const App = () => {
     <div className="container">
       <div className="row">
         <div className="col-12">
-          <div className="fs-1 d-flex justify-content-center">Todo List</div>
+          <div className="fs-1 d-flex justify-content-center">Todo Listado</div>
           <form onSubmit={addTask}>
             <div className="mb-3">
               <input
