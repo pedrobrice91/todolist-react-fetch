e67fb021f4e3e24a68f2472ed3c041d706114207
Pedro L Briceno <66653154+pedrobrice91@users.noreply.github.com>
2024-07-27 15:54:45 -0400

    finish

--- src/App.js
+++ src/App.js
@@ -1,60 +1,118 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
 const App = () => {
-  const [state, setState] = useState({ labels: "" });
-  const [list, setList] = useState([]);
+	const [state, setState] = useState({label:'', is_done:false});
+	const [list, setList] = useState([]);
 
-  const cambios = (e) => {
-    setState({
-      ...state,
-      [e.target.name]: e.target.value,
-    });
-  };
+	const cambios = (e) => {
+		setState(
+			{...state, [e.target.name]: e.target.value}
+		);
+	};
 
-  const addTask = (e) => {
-    e.preventDefault();
-    if (state.labels.trim() !== "") {
-      setList([...list, state.labels]);
-      setState({ labels: "" }); // Resetea el campo de input
-    }
-  };
+	const consulta = async () => {
+		try {
+			const response = await fetch(
+				"https://playground.4geeks.com/todo/users/pedrobriceno",
+				{
+					method: "GET",
+					headers: { "Content-type": "application/json"}
+				},
+			);
+			const data = await response.json();
+			setList(data.todos);
+			console.log(data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  const removeTask = (index) => {
-    setList(list.filter((_, i) => i !== index));
-  };
+	const guardo = async (tarea) => {
+		try {
+			const response = await fetch(
+				"https://playground.4geeks.com/todo/todos/pedrobriceno",
+				{
+					method: "POST",
+					headers: { "Content-type": "application/json", 'accept': 'application/json' },
+					body: JSON.stringify(tarea),
+				},
+			);
+			const data = await response.json();
+			console.log(data);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-12">
-          <div className="fs-1 d-flex justify-content-center">Todo Listados</div>
-          <form onSubmit={addTask}>
-            <div className="mb-3">
-              <input
-                onChange={cambios}
-                name="labels"
-                type="text"
-                className="form-control"
-                placeholder="Agrega tu tarea"
-                value={state.labels}
-              />
-            </div>
-          </form>
-          <ul className="list-group list-group-flush">
-            {list.map((task, index) => (
-              <li key={index} className="list-group-item d d-flex justify-content-between">
-                <span className="p-2 w-100">{task}</span>
-                <button type="button" className="btn btn-danger" onClick={() => removeTask(index)}>
-                  Eliminar
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+	const eliminar = async (id) => {
+		try {
+			const response = await fetch(
+				"https://playground.4geeks.com/todo/todos/"+id,
+				{
+					method: "DELETE",
+					headers: { "Content-type": "application/json" },
+				},
+			);
+			if (response.status === 204) {
+				consulta()
+			}
+		} catch (error) {
+			console.log(error);
+		}
+	};
+
+	useEffect(() => {
+		consulta();
+	}, []);
+
+	const addTask = (e) => {
+		e.preventDefault();
+		if (state.label.trim() !== "") {
+			setList([...list, state]);
+			setState({label:'', is_done:false}); 
+			guardo(state)
+		}
+	};
+
+	return (
+		<div className="container">
+			<div className="row">
+				<div className="col-12">
+					<div className="fs-1 d-flex justify-content-center">Listado</div>
+					<form onSubmit={addTask}>
+						<div className="mb-3">
+							<input
+								onChange={cambios}
+								name="label"
+								type="text"
+								className="form-control"
+								placeholder="Agrega tu tarea"
+								value={state.label}
+							/>
+						</div>
+					</form>
+					<ul className="list-group list-group-flush">
+						{list.map((task, index) => (
+							<li
+								key={index}
+								className="list-group-item d d-flex justify-content-between"
+							>
+								<span className="p-2 w-100">{task.label}</span>
+								<button
+									type="button"
+									className="btn btn-danger"
+									onClick={() => eliminar(task.id)}
+								>
+									Eliminar
+								</button>
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default App;
